aaf9f39d52e74247e6c9d401180a95fb75e36d42
Jaycar-Electronics <43460991+Jaycar-Electronics@users.noreply.github.com>
2020-07-28 17:44:09 +1000

    Update robotCode.ino

--- robotCode/robotCode.ino
+++ robotCode/robotCode.ino
@@ -1,136 +1,169 @@
-// Wall Doding robot code
-// Jaycar Electronics 2020
+//Ultrasonic sensor pins
+#define TRIG 9
+#define ECHO 10
 
-#include <AFMotor.h>
+//motor module pins
+const int channel_a_enable  = 6;
+const int channel_a_input_1 = 4;
+const int channel_a_input_2 = 7;
+const int channel_b_enable  = 5;
+const int channel_b_input_3 = 3;
+const int channel_b_input_4 = 2;
 
-const int ldr_pin = A5;
-const int trigger_pin = A2;
-const int echo_pin = A3;
+int a=0;    //motor a speed and direction
+int b=0;    //motor b speed and direction
+int phase=0;    //what stage are we up to
+long timeout=0;    //how long have we been doing it for
 
-const int light_threshold = 700;
-const int sonar_threshold = 200;
-
-AF_DCMotor left_motor(4);
-AF_DCMotor right_motor(3);
-
-//define the functions so we can use them later.
+#define PROX 30
+#define FORWARDSPEED 150
+#define REVERSESPEED 150
+//set TURNSPEED negative to turn the other way
+#define TURNSPEED 150
+#define REVERSETIME 1000
+#define TURNTIME 1000
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("Robot Starting!");
+  pinMode(11,OUTPUT);
+  digitalWrite(11,LOW);    //GND for ultrasonic
+  pinMode(8,OUTPUT);
+  digitalWrite(8,HIGH);    //5V for ultrasonic
+  
+  usonicsetup();
+  
+  pinMode( channel_a_enable, OUTPUT );  // Channel A enable
+  pinMode( channel_a_input_1, OUTPUT ); // Channel A input 1
+  pinMode( channel_a_input_2, OUTPUT ); // Channel A input 2
+  
+  pinMode( channel_b_enable, OUTPUT );  // Channel B enable
+  pinMode( channel_b_input_3, OUTPUT ); // Channel B input 3
+  pinMode( channel_b_input_4, OUTPUT ); // Channel B input 4
+//everything off
+  digitalWrite( channel_a_input_1, LOW);
+  digitalWrite( channel_a_input_2, LOW);
+  digitalWrite( channel_a_enable, LOW);
 
-  pinMode(ldr_pin, INPUT);
-  pinMode(trigger_pin, OUTPUT);
-  pinMode(echo_pin, INPUT);
-
-  Serial.println("Robot OK!");
+  digitalWrite( channel_b_input_3, LOW);
+  digitalWrite( channel_b_input_4, LOW);
+  digitalWrite( channel_b_enable, LOW);
+  
+  delay(1000);  //wait a bit
+  
+  Serial.begin( 9600 );    //serial debug
+  Serial.println("Starting up");
 }
 
 void loop()
 {
-  delay(100);
+  //read ultrasonic sensor
+  int d;
+  d=usonic(11600)/58;  // distance in cm
+  if(d==0){d=200;}
+  Serial.println(d);    //print d on serial monitor
 
-  int ldrReading = analogRead(ldr_pin);
-  long sonarDistance = sonarPing();
+  //react based on d
+  switch(phase){
+    case 0:
+      if(d<PROX){phase=1;timeout=millis();}    //if too close, reverse
+      break;
+    case 1:
+      if(millis()>timeout+REVERSETIME){phase=2;timeout=millis();}    //reversed for REVERSETIME, now turn
+      break;
+    case 2:
+      if(millis()>timeout+TURNTIME){phase=0;}    //turned for TURNTIME, now back to forward
+      break;
+  }
+  
+  //print phase for debugging
+  Serial.print("PHASE:");
+  Serial.println(phase);
 
-  Serial.print("LDR Reading: ");
-  Serial.println(ldrReading, DEC);
-  Serial.print("Distance Reading: ");
-  Serial.println(sonarDistance, DEC);
+  //set motors based on stage
+  switch(phase){
+    case 0:    //normal going forward
+      a=FORWARDSPEED;
+      b=FORWARDSPEED;
+      break;
+    case 1:    //backing up
+      a=-(REVERSESPEED);
+      b=-(REVERSESPEED);
+      break;
+    case 2:    //turn a little
+      a=TURNSPEED;
+      b=-(TURNSPEED);
+      break;
+    default:      //something wrong happened
+      a=0;
+      b=0;
+      break;
+  }
+  setmotor();    
+  delay(200);    //wait a bit
+}
 
-  //check if the lights are on
-  if (ldrReading < light_threshold)
-  {
-    stop();
-    blinkErrorMessage();
-    //return as we don't want to do more in the loop function
-    return;
+void setmotor(){
+  int s;
+  
+  if(a>0){
+      digitalWrite( channel_a_input_1, HIGH);
+      digitalWrite( channel_a_input_2, LOW);
+  }
+  if(a<0){
+      digitalWrite( channel_a_input_1, LOW);
+      digitalWrite( channel_a_input_2, HIGH);
+  }
+  if(b>0){
+      digitalWrite( channel_b_input_3, HIGH);
+      digitalWrite( channel_b_input_4, LOW);
+  }
+  if(b<0){
+      digitalWrite( channel_b_input_3, LOW);
+      digitalWrite( channel_b_input_4, HIGH);
   }
 
-  // check distance of what's in front of us
-  // (closer objects are lower values)
-  if (sonarDistance < sonar_threshold)
-  {
-    //turn around;
-    stop();
-    driveBackward(800);
-    reverseTurn(800);
-    stop();
-  }
+  s=abs(a);
+  if(s>255){s=255;}
+  if(s<0){s=0;}
+  analogWrite( channel_a_enable, s);
+  
+  s=abs(b);
+  if(s>255){s=255;}
+  if(s<0){s=0;}
+  analogWrite( channel_b_enable, s);
+  
+}  
 
-  //drive forward
-  driveForward();
-}
-// ===============================================================
 
-// ----------------------------------------------------
-// blinkErrorMessage function
-//   This is to just blink the onboard LED when needed.
-
-void blinkErrorMessage()
+void allInputsOff()
 {
+  digitalWrite( channel_a_input_1, LOW);
+  digitalWrite( channel_a_input_2, LOW);
+  digitalWrite( channel_a_enable, LOW);
 
-  pinMode(13, OUTPUT);
-
-  digitalWrite(13, HIGH);
-  delay(200);
-  digitalWrite(13, LOW);
-
-  delay(100);
-
-  digitalWrite(13, HIGH);
-  delay(200);
-  digitalWrite(13, LOW);
-
-  delay(1000);
-  pinMode(13, INPUT);
+  digitalWrite( channel_b_input_3, LOW);
+  digitalWrite( channel_b_input_4, LOW);
+  digitalWrite( channel_b_enable, LOW);
 }
 
-// --------------------------------------------------------
-// motion functions
-//   These just set the correct motor values to drive the bot
-
-void driveForward()
-{
-  left_motor.run(FORWARD);
-  right_motor.run(FORWARD);
-  left_motor.setSpeed(255);
-  right_motor.setSpeed(255);
-}
-void driveBackward(long time)
-{
-  left_motor.run(BACKWARD);
-  right_motor.run(BACKWARD);
-  left_motor.setSpeed(255);
-  right_motor.setSpeed(255);
-  delay(time); //drive backwards for half a second
-}
-void stop()
-{
-  left_motor.setSpeed(0);
-  right_motor.setSpeed(0);
-  delay(500); //stop for half a second, allow motors to wind down
-}
-void reverseTurn(long time)
-{
-  left_motor.run(FORWARD);
-  right_motor.run(BACKWARD);
-  left_motor.setSpeed(0);
-  right_motor.setSpeed(255);
-  delay(time);
+void usonicsetup(void){
+  pinMode(ECHO, INPUT);
+  pinMode(TRIG, OUTPUT);
+  digitalWrite(TRIG, LOW);
 }
 
-// -------------------------------------------------------------
-// sonarPing function
-//    This measures the distance (in milimeters) to the object infront of it
-unsigned long sonarPing()
-{
-  //send a pulse and time how long it takes to come back.
-  digitalWrite(trigger_pin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger_pin, LOW);
-
-  unsigned long duration = pulseIn(echo_pin, HIGH);
-  return duration * 0.17; //convert microseconds to millimeters; (speed of sound);
+long usonic(long utimeout){    //utimeout is maximum time to wait for return in us
+  long b;
+  if(digitalRead(ECHO)==HIGH){return 0;}    //if echo line is still low from last result, return 0;
+  digitalWrite(TRIG, HIGH);  //send trigger pulse
+  delay(1);
+  digitalWrite(TRIG, LOW);
+  long utimer=micros();
+  while((digitalRead(ECHO)==LOW)&&((micros()-utimer)<1000)){}  //wait for pin state to change- return starts after 460us typically
+  utimer=micros();
+  while((digitalRead(ECHO)==HIGH)&&((micros()-utimer)<utimeout)){}  //wait for pin state to change
+  b=micros()-utimer;
+  return b;
 }
+ 
+
